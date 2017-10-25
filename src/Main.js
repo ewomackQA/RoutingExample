4f1c54cdbc2a3487de06c17325b3460f3d569efd
Womack <EWomack@corp.group.local>
2017-10-25 16:41:54 +0100

    cleaned up some smells

--- src/Main.js
+++ src/Main.js
@@ -11,27 +11,22 @@ const BasicExample = () => (
         <div>
             <ul>
                 <li><Link to="/">Home</Link></li>
-                <li><Link to="/about">About</Link></li>
                 <li><Link to="/topics">Topics</Link></li>
                 <li><Link to="/formexample">FormExample</Link></li>
-
             </ul>
-
             <hr />
-
             <Route exact path="/" component={Home} />
             <Route path="/topics" component={Topics} />
             <Route path="/formexample" component={FormExample} />
-
         </div>
     </Router>
-)
+);
 
 const Home = () => (
     <div>
         <h2>Home</h2>
     </div>
-)
+);
 
 
 const Topics = ({ match }) => (
@@ -61,12 +56,12 @@ const Topics = ({ match }) => (
             <h3>Please select a topic.</h3>
         )} />
     </div>
-)
+);
 
 const Topic = ({ match }) => (
     <div>
         <h3>{match.params.topicId}</h3>
     </div>
-)
+);
 
 export default BasicExample;
